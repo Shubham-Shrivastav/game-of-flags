e22d73ecbd7e3d508ca584939cf19d96ed17b4d7
Shubham-Shrivastav <shuboom@969@gmail.com>
2023-05-03 12:07:26 +0530

    Added flags changing feature

--- src/App.js
+++ src/App.js
@@ -1,34 +1,54 @@
-// import logo from './logo.svg';
-
 import nations from './nations'
 import '../node_modules/flag-icon-css/css/flag-icons.css';
 import './App.css';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 
   const [country, setCountry] = useState([]);
+  const [flagCountry, setFlagCountry] = useState({});
+
   const generateRandomNations = () => {
     let ct = [];
     for (let i = 0; i < 4; i++) {
       const r = Math.floor(Math.random() * nations.length);
       ct.push(nations[r]);
     }
-    console.log(country);
+
     setCountry(ct);
+    const index = Math.floor(Math.random() * 4);
+    setFlagCountry(ct[index]);
+  };
+
+  const checkAnswer = (c) => {
+    if (c.name === flagCountry.name) {
+      alert('Correct Answer');
+    } else {
+      alert('Incorrect');
+    }
+    generateRandomNations();
   }
+
   useEffect(() => {
     generateRandomNations();
-
-  }, [])
+  }, []);
 
   return (
     <div className="App">
-      <span className="flag-icon flag-icon-gr"></span>
-      <div>{
-        country.map(c => <button key={c.alpha2Code}>{c.name}</button>)}</div>
+      {flagCountry.name ? (
+        <span className={`flag-icon flag-icon-${flagCountry['alpha-2'].toLowerCase()}`}>
+
+        </span>) : null}
+
+      <div>
+        {
+          country.map(c => <button key={c.name} onClick={() => checkAnswer(c)}>
+            {c.name}
+          </button>)
+        }
+      </div>
     </div>
   );
 }
+
 export default App;
